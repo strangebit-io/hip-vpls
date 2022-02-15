04921bb309267aa4cb4b1a4838aab5acaa94e875
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-02-15 21:53:03 +0500

    removed junk

--- hiplib/report/microbanchmarking/duration_bex.R
+++
@@ -1,5 +0,0 @@
-d<-scan("duration_bex.dat")
-pdf("duration_bex.pdf", height=4)
-hist(d, col="dark red", xlab="BEX duration, seconds", ylab="Frequency", main="");
-grid(col="black", lwd=2)
-dev.off()
