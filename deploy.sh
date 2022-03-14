3e247f6036a601a34f3fad9afa1bf62ac001652e
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-03-14 22:38:14 +0500

    Cleaning the folders

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
+# Removing unused folder
+sudo rm -rf mininet
+
+# Cloning the database
 git clone git://github.com/mininet/mininet
 cd mininet
 git tag  # list available versions
 git checkout -b mininet-2.3.0 2.3.0  # or whatever version you wish to install
 cd ..
 
-bash sudo PYTHON=python3 mininet/util/install.sh -n
+# Installing the mininet globally
+sudo PYTHON=python3 mininet/util/install.sh -a
+
+# Testing the mininet installation
 sudo mn --switch ovsbr --test pingall
 
 
