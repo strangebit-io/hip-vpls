7d00a800bb1c8b4dcfca3c1f0f66d432bd5ed967
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2024-01-25 23:40:41 +0500

    Debug all

--- router4/switchd.py
+++ router4/switchd.py
@@ -24,6 +24,8 @@ __email__ = "dmitriy.kuptsov@gmail.com"
 __status__ = "development"
 
 # Import the needed libraries
+# OS library
+import os
 # Stacktrace
 import traceback
 # Sockets
@@ -32,6 +34,7 @@ import socket
 import threading
 # Logging
 import logging
+from logging.handlers import RotatingFileHandler
 # Timing
 import time
 # Math functions
@@ -50,6 +53,12 @@ from binascii import hexlify
 # Import HIP library
 from hiplib import hlib
 
+# Utilities
+from hiplib.utils import misc
+
+# Crypto stuff
+from hiplib.crypto import digest
+
 from hiplib.hlib import HIPLib
 
 # HIP related packets
@@ -62,20 +71,30 @@ from hiplib.packets import IPv6
 from hiplib.packets import IPv4
 # Ethernet frame
 from hiplib.packets import Ethernet
+# Controller packets
+from hiplib.packets import Controller
 # Configuration
 from hiplib.config import config as hip_config
 # Import switch FIB
 from switchfabric import FIB
+
+# Network stuff
+import socket
+import ssl
+
+# HIP controller lock
+hip_config_socket_lock = threading.Lock()
+
 
 # Copy routines
 import copy
 
 # Configure logging to console and file
 logging.basicConfig(
-    level=logging.CRITICAL,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        #logging.FileHandler("hipls.log")#,
+        RotatingFileHandler("hipls.log"),
         logging.StreamHandler(sys.stdout)
     ]
 );
@@ -90,27 +109,244 @@ hip_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1);
 logging.info("Initializing IPSec socket");
 ip_sec_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, IPSec.IPSEC_PROTOCOL);
 #ip_sec_socket.bind(("0.0.0.0", IPSec.IPSEC_PROTOCOL));
-ip_sec_socket.bind((hip_config.config["swtich"]["source_ip"], IPSec.IPSEC_PROTOCOL))
+ip_sec_socket.bind((hip_config.config["switch"]["source_ip"], IPSec.IPSEC_PROTOCOL))
 
 # We will need to perform manual fragmentation
 ip_sec_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1);
 # Open raw ethernet socket and bind it to the interface
 ETH_P_ALL = 3
 ether_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL));
-ether_socket.bind((hip_config.config["swtich"]["l2interface"], 0))
+ether_socket.bind((hip_config.config["switch"]["l2interface"], 0))
 
 # Initialize FIB
-fib = FIB(hip_config.config["swtich"]["mesh"])
+fib = FIB(hip_config.config["switch"]["mesh"])
+
+# Load MAC ACL rules
+fib.load_rules(hip_config.config["firewall"]["acl_file"])
+
+hip_config_socket = None
 
 def onclose():
     packets = hiplib.exit_handler()
     for (packet, dest) in packets:
         hip_socket.sendto(packet, dest)
 
+def open_controller_socket():
+    """
+    Conncects to controller in the cloud
+    """
+    ctx = ssl.create_default_context();
+    ctx.load_verify_locations(hip_config.config["controller"]["ca_cert"]);
+    ip = socket.gethostbyname(hip_config.config["controller"]["controller_host_name"])
+    sock = socket.create_connection((ip, hip_config.config["controller"]["controller_port"]));
+    ctx.check_hostname = True;
+    secure_socket = ctx.wrap_socket(sock, server_hostname=hip_config.config["controller"]["controller_host_name"], 
+                                    server_side=False);
+    return secure_socket
+
+def write_rules_to_file(rules):
+    fd = open(hip_config.config["firewall"]["rules_file"], "w")
+    for rule in rules:
+        hit1 = misc.Utils.ipv6_bytes_to_hex_formatted_resolver(rule["hit1"])
+        hit2 = misc.Utils.ipv6_bytes_to_hex_formatted_resolver(rule["hit2"])
+        if rule["rule"] == 1:
+            rule = "allow"
+        else:
+            rule = "deny"
+        fd.write(hit1 + " " + hit2 + " " + rule + "\n")
+    fd.close();
+    hiplib.firewall.load_rules(hip_config.config["firewall"]["rules_file"])
+
+def write_mesh_to_file(mesh):
+    fd = open(hip_config.config["switch"]["mesh"], "w")
+    for m in mesh:
+        hit1 = misc.Utils.ipv6_bytes_to_hex_formatted_resolver(m["hit1"])
+        hit2 = misc.Utils.ipv6_bytes_to_hex_formatted_resolver(m["hit2"])
+        fd.write(hit1 + " " + hit2 + "\n")
+    fd.close();
+    fib.load_mesh(hip_config.config["switch"]["mesh"])
+
+def write_hosts_to_file(hosts):
+    fd = open(hip_config.config["resolver"]["hosts_file"], "w")
+    for host in hosts:
+        hit = misc.Utils.ipv6_bytes_to_hex_formatted_resolver(host["hit"])
+        ip = misc.Utils.ipv4_bytes_to_string(host["ip"])
+        fd.write(hit + " " + ip + "\n")
+    fd.close();
+    hiplib.hit_resolver.load_records(hip_config.config["resolver"]["hosts_file"])
+
+def write_acl_to_file(rules):
+    fd = open(hip_config.config["firewall"]["acl_file"], "w")
+
+    logging.debug("WRITING TO FILE ACL RULES.....")
+    logging.debug("NUMBER OF RULES %d" % (len(rules)))
+
+    for rule in rules:
+
+        mac1 = misc.Utils.mac_bytes_to_hex_formatted(rule["mac1"])
+        mac2 = misc.Utils.mac_bytes_to_hex_formatted(rule["mac2"])
+        logging.debug(mac1)
+        logging.debug(mac2)
+
+        if rule["rule"] == 1:
+            rule = "allow"
+        else:
+            rule = "deny"
+
+        fd.write(mac1 + " " + mac2 + " " + rule + "\n")
+    fd.close();
+    fib.load_rules(hip_config.config["firewall"]["acl_file"])
+
+def config_loop():
+    buf = bytearray([])
+    global hip_config_socket
+    global hip_config_socket_lock
+    while True:
+        logging.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        if not hip_config_socket:
+            buf = bytearray([])
+            sleep(hip_config.config["controller"]["heartbeat_interval"]);
+            continue
+        buf_ = bytearray([])
+        try:
+            buf_ = bytearray(hip_config_socket.recv(hip_config.config["controller"]["default_buffer"]))
+            buf += buf_
+        except:
+            hip_config_socket = None
+            buf = bytearray([])
+            sleep(hip_config.config["controller"]["heartbeat_interval"]);
+            continue
+        if len(buf_) == 0:
+            hip_config_socket = None
+            buf = bytearray([])
+            sleep(hip_config.config["controller"]["heartbeat_interval"]);
+            continue
+        logging.debug("GOT SOMETHING ON THE CONTROL INTERFACE");
+        while len(buf) >= Controller.BASIC_HEADER_OFFSET:
+            packet = Controller.ControllerPacket(buf)
+            logging.debug("PACKET TYPE **************************************** " + str(packet.get_packet_type()))
+            if packet.get_packet_length() > len(buf):
+                break
+            pbuf = buf[:packet.get_packet_length()]
+            buf = buf[packet.get_packet_length():]
+            logging.debug("+++++++++++++++++++++++++++++++++++++++++++++++")
+            logging.debug(packet.get_packet_type())
+            
+            if packet.get_packet_type() == Controller.FIREWALL_CONFIGURATION_TYPE:
+                packet = Controller.FirewallConfigurationPacket(pbuf)
+                hmac = packet.get_hmac()
+                packet.set_hmac([0]*digest.SHA256Digest.LENGTH)
+                sha256hmac = digest.SHA256HMAC(bytearray(hip_config.config["controller"]["master_secret"], encoding="ascii"))            
+                if hmac != sha256hmac.digest(packet.get_buffer()):
+                    logging.critical("Invalid HMAC in the packet")
+                    continue
+                rules = packet.get_rules()
+                write_rules_to_file(rules)
+            elif packet.get_packet_type() == Controller.MESH_CONFIGURATION_TYPE:
+                packet = Controller.MeshConfigurationPacket(pbuf)
+                hmac = packet.get_hmac()
+                packet.set_hmac([0]*digest.SHA256Digest.LENGTH)
+                logging.debug("packet.get_buffer()")
+                logging.debug(packet.get_buffer())
+                logging.debug(hexlify(packet.get_nonce()))
+                sha256hmac = digest.SHA256HMAC(bytearray(hip_config.config["controller"]["master_secret"], encoding="ascii"))            
+                if hmac != sha256hmac.digest(packet.get_buffer()):
+                    logging.critical("Invalid HMAC in the packet")
+                    continue
+                mesh = packet.get_mesh();
+                logging.debug(mesh)
+                write_mesh_to_file(mesh)
+            elif packet.get_packet_type() == Controller.HOSTS_CONFIGURATION_TYPE:
+                logging.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                packet = Controller.HostsConfigurationPacket(pbuf)
+                hmac = packet.get_hmac()
+                packet.set_hmac([0]*digest.SHA256Digest.LENGTH)
+                sha256hmac = digest.SHA256HMAC(bytearray(hip_config.config["controller"]["master_secret"], encoding="ascii"))           
+                if hmac != sha256hmac.digest(packet.get_buffer()):
+                    logging.critical("Invalid HMAC in the packet")
+                    continue;
+                logging.debug("Writing to the file >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                hosts = packet.get_hosts()
+                logging.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                write_hosts_to_file(hosts)
+            elif packet.get_packet_type() == Controller.ACL_CONFIGURATION_TYPE:
+                logging.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                packet = Controller.ACLConfigurationPacket(pbuf)
+                hmac = packet.get_hmac()
+                packet.set_hmac([0]*digest.SHA256Digest.LENGTH)
+                sha256hmac = digest.SHA256HMAC(bytearray(hip_config.config["controller"]["master_secret"], encoding="ascii"))           
+                if hmac != sha256hmac.digest(packet.get_buffer()):
+                    logging.critical("Invalid HMAC in the packet")
+                    continue;
+                logging.debug("Writing to the file >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                rules = packet.get_rules()
+                logging.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                write_acl_to_file(rules)
+            else:
+                logging.debug("Invalid control-plane packet type")
+
+def heart_beat_loop():
+    """
+    Heart beat loop: 
+        send periodic registration messages to the cloud (this includes HIT and IP address)
+    """
+    global hip_config_socket
+    global hip_config_socket_lock
+    while True:
+        try:
+            hip_config_socket_lock.acquire();
+            if not hip_config_socket:
+                hip_config_socket = open_controller_socket();
+        except Exception as e:
+            logging.debug(e)
+            logging.debug("Error!!!!")
+            sleep(hip_config.config["controller"]["heartbeat_interval"]);
+            continue
+        finally:
+            hip_config_socket_lock.release();
+        nonce = os.urandom(4);
+        hit = hiplib.get_own_hit();
+        ip = misc.Utils.ipv4_to_bytes(hip_config.config["switch"]["source_ip"]);
+        heartbeat = Controller.HeartbeatPacket();
+        heartbeat.set_packet_type(Controller.HEART_BEAT_TYPE);
+        heartbeat.set_hit(hit);
+        heartbeat.set_ip(ip);
+        heartbeat.set_packet_length(Controller.HEART_BEAT_LENGTH_LENGTH);
+        heartbeat.set_nonce(nonce);
+        hostname = hip_config.config["controller"]["switch_name"].encode("ascii")
+        heartbeat.set_hostname_length(len(hostname))
+        heartbeat.set_hostname(hostname, len(hostname))
+        buf = heartbeat.get_buffer();
+        hmac = digest.SHA256HMAC(bytearray(hip_config.config["controller"]["master_secret"], encoding="ascii"))
+        hmac_ = hmac.digest(buf)
+        heartbeat.set_hmac(hmac_)
+        """
+        logging.debug("---------------------")
+        logging.debug(hexlify(buf))
+        logging.debug(bytearray(hip_config.config["controller"]["master_secret"], encoding="ascii"))
+        logging.debug(hexlify(heartbeat.get_buffer()))
+        logging.debug("Nonce : " + str(hexlify(heartbeat.get_nonce())))
+        logging.debug("HIT: " + str(hexlify(heartbeat.get_hit())))
+        logging.debug("IP: " + str(hexlify(heartbeat.get_ip())))
+        logging.debug("MAC: " + str(hexlify(heartbeat.get_hmac())))
+        logging.debug("++++++++++++++++++++++")
+        """
+        bytes_sent = hip_config_socket.send(heartbeat.get_buffer());
+        if bytes_sent == 0:
+            try:
+                hip_config_socket_lock.acquire();
+                if not hip_config_socket:
+                    hip_config_socket = open_controller_socket();
+            except:
+                pass
+            finally:
+                hip_config_socket_lock.release();
+        sleep(hip_config.config["controller"]["heartbeat_interval"]);
+
 def hip_loop():
     while True:
         try:
-            logging.debug("Got HIP packet on the interface")
+            #logging.debug("Got HIP packet on the interface")
             packet = bytearray(hip_socket.recv(1518))
             packets = hiplib.process_hip_packet(packet);
             for (packet, dest) in packets:
@@ -118,6 +354,7 @@ def hip_loop():
         except Exception as e:
             logging.debug("Exception occured while processing HIP packet")
             logging.debug(e)
+            logging.debug(traceback.format_exc())
 
 def ip_sec_loop():
     while True:
@@ -143,6 +380,7 @@ def ip_sec_loop():
             ee = time()
             #logging.info("Total time to process the IPSEC packet %f" % (ee - es))
         except Exception as e:
+            logging.debug("Exception occured while processing IPSEC packet")
             logging.critical(e)
 
 def ether_loop():
@@ -163,26 +401,29 @@ def ether_loop():
 
             #logging.debug("----------------------------------")
             es = time()
+            
+            # Check ACL
+            if not fib.is_allowed(hexlify(src_mac).decode("ascii"), hexlify(dst_mac).decode("ascii")):
+                continue
             mesh = fib.get_next_hop(dst_mac);
             for (ihit, rhit) in mesh:
                 s = time()
-                packets = hiplib.process_l2_frame(frame, ihit, rhit, hip_config.config["swtich"]["source_ip"]);
+                packets = hiplib.process_l2_frame(frame, ihit, rhit, hip_config.config["switch"]["source_ip"]);
                 e = time()
                 #logging.info("L2 process time %f " % (e-s))
-                #for (hip, packet, dest) in packets:
-                (hip, packet, dest) = packets[0]
-                #logging.debug("Sending L2 frame to: %s %s" % (hexlify(ihit), hexlify(rhit)))
-                if not hip:
-                    s = time()
-                    ip_sec_socket.sendto(packet, dest)
-                    e = time()
-                    #logging.info("IPSEC send time %f " % (e-s))
-                else:
-                    hip_socket.sendto(packet, dest)
+                for (hip, packet, dest) in packets:
+                    #logging.debug("Sending L2 frame to: %s %s" % (hexlify(ihit), hexlify(rhit)))
+                    if not hip:
+                        s = time()
+                        ip_sec_socket.sendto(packet, dest)
+                        e = time()
+                        #logging.info("IPSEC send time %f " % (e-s))
+                    else:
+                        hip_socket.sendto(packet, dest)
             ee = time()
             #logging.info("Total time to process Ethernet frame %f" % (ee-es))
         except Exception as e:
-           #logging.debug("Exception occured while processing L2 frame")
+           logging.debug("Exception occured while processing L2 frame")
            logging.debug(e)
 
 
@@ -192,23 +433,28 @@ atexit.register(onclose);
 hip_th_loop = threading.Thread(target = hip_loop, args = (), daemon = True);
 ip_sec_th_loop = threading.Thread(target = ip_sec_loop, args = (), daemon = True);
 ether_if_th_loop = threading.Thread(target = ether_loop, args = (), daemon = True);
+heart_beat_th_loop = threading.Thread(target = heart_beat_loop, args = (), daemon = True);
+config_th_loop = threading.Thread(target = config_loop, args = (), daemon = True);
 
 logging.info("Starting the CuteHIP");
 
 hip_th_loop.start();
 ip_sec_th_loop.start();
 ether_if_th_loop.start();
+heart_beat_th_loop.start();
+config_th_loop.start();
 
-def run_swtich():
+def run_switch():
     while True:
         try:
             packets = hiplib.maintenance();
             for (packet, dest) in packets:
                 hip_socket.sendto(packet, dest)
-            logging.debug("...Periodic cleaning task...")
+            #logging.debug("...Periodic cleaning task...")
             sleep(1);
         except Exception as e:
             logging.critical("Exception occured while processing HIP packets in maintenance loop")
             logging.critical(e);
+            sleep(1)
 
-run_swtich()
+run_switch()
