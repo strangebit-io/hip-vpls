765f7129d17a7f229cd517d46fad7e8765c925e7
André Koch <andre@mokan.se>
2024-11-12 11:57:54 +0100

    finished setup script

--- setup_spokehub.sh
+++ setup_spokehub.sh
@@ -5,17 +5,19 @@ s2="/spoke2/hiplib"
 s3="/spoke3/hiplib"
 h1="/hub1/hiplib"
 h2="/hub2/hiplib"
-h2="/hub3/hiplib"
+h3="/hub3/hiplib"
 
-
-#
-# SPOKES
-#
 
 # Copy hlib.py file from spoke 1 to spoke 2 3
 for i in ${project}${s2}/hlib.py ${project}${s3}/hlib.py
 do 
 	sudo cp ${project}${s1}/hlib.py $i
+done 
+
+# Copy hlib.py file from hub 1 to hub 2 3
+for i in ${project}${h2}/hlib.py ${project}${h3}/hlib.py
+do 
+	sudo cp ${project}${h1}/hlib.py $i
 done 
 
 # Copy switchd.py file from spoke 1 to spoke 2 3
@@ -24,24 +26,21 @@ do
 	sudo cp ${project}/spoke1/switchd.py $i
 done 
 
-#
-# HUBS
-#
-
-# Copy hlib.py file from hub 1 to hub 2 3
-for i in ${project}${h2}/hlib.py ${project}${h3}/hlib.py
-do 
-	sudo cp ${project}${h1}/hlib.py $i
-done 
-hub
 # Copy switchd.py file from hub 1 to hub 2 3
 for i in ${project}/hub2/switchd.py ${project}/hub3/switchd.py
 do 
 	sudo cp ${project}/hub1/switchd.py $i
-done 
+done
 
+# Copy relevant folders from spoke 1 to spoke 2 3
+for f in crypto packets databases
+do
+	for i in ${project}${s2}/${f} ${project}${s3}/${f}
+	do 
+		sudo cp -r -T ${project}${s1}/${f} $i
+	done 
+done
 
-# Copy relevant folders from hub 1 to hub 2 3
 for f in crypto packets databases
 do
 	for i in ${project}${h2}/${f} ${project}${h3}/${f}
@@ -49,3 +48,4 @@ do
 		sudo cp -r -T ${project}${h1}/${f} $i
 	done 
 done
+echo "Finished copying hubs & spokes."
